455f252260c1169c36722cf663151abe6384e8b4
Stefan Hanauska <stefan.hanauska@csg-in.de>
2025-10-04 16:14:04 +0200

    Add release workflow

--- version.php
+++ version.php
@@ -28,5 +28,5 @@ $plugin->component = 'mod_kanban';
 $plugin->release = '0.3.3';
 $plugin->version = 2025080701;
 $plugin->requires = 2022112800;
-$plugin->supported = [401, 500];
+$plugin->supported = [401, 501];
 $plugin->maturity = MATURITY_STABLE;
